b6c1d6977cb78074f1c756e5bc27d29ad8873de2
gabriel.gomes <you@example.com>
2018-02-06 15:46:24 -0200

    protect bad geojson

--- controllers/osm.js
+++ controllers/osm.js
@@ -284,6 +284,9 @@ function getWays(i, lastId) {
             var bulk = [];
             async.each(result, function (r, callback) {
                 var nearWay = r.toJSON();
+                if(nearWay.loc.coordinates[0][0] == nearWay.loc.coordinates[1][0] && nearWay.loc.coordinates[1][1] == nearWay.loc.coordinates[1][1]){
+                    return callback();
+                }
                 var retorno = {_id: nearWay._id, osm_type: "way", loc: nearWay.loc, nodes: nearWay.nodes};
                 retorno.address = {
                     road: nearWay.tags.name ? nearWay.tags.name : nearWay.tags.description,
